574471cef4ca2869442f055c3ebe69daee2a3b09
JonnyMe <jonathan.albrieux@gmail.com>
2023-04-21 17:11:10 +0200

    add route for authentication

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 #[macro_use] extern crate rocket;
+mod auth;
+mod user;
+
+use user::User;
+use auth::Auth;
+use rocket::form::Form;
 
 #[get("/")]
 fn index() -> &'static str {
     "Ciao Giovini!"
 }
 
+#[post("/authenticate", data = "<user>")]
+fn authenticate(user: Form<User<'_>>) -> String {
+    Auth::new(user.username.to_string()).token()
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, authenticate])
 }
